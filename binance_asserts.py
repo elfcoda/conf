db4460d8a867cf4ba3c7e48b1da1cd0df1f36126
elfcoda <30581589+elfcoda@users.noreply.github.com>
2024-03-18 19:15:07 +0800

    Update binance_asserts.py

--- binance_asserts.py
+++ binance_asserts.py
@@ -1,8 +1,9 @@
 {
-  info: ["karsms@icloud.com", "15574989048"],
+  info: ["karsms@icloud.com",  "15574989048"],
   sub: {
     "BTC": [100, [31511.0], True, False],
     "OCEAN": [1.22, [1.2], False, False],
     "PEPE": [200, [0.00000830], False, False]
   }
 }
+
