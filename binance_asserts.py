fac11b3739c36cce0d52499fd7cc6d7af4f7efe8
elfcoda <30581589+elfcoda@users.noreply.github.com>
2024-03-18 18:03:33 +0800

    Update binance_asserts.py

--- binance_asserts.py
+++ binance_asserts.py
@@ -1,5 +1,8 @@
 {
-  "BTC": [100, [31511.0], True, False],
-  "OCEAN": [1.22, [1.2], False, False],
-  "PEPE": [200, [0.00000830], False, False]
+  info: ["karsms@icloud.com", "15574989048"]  
+  sub: {
+    "BTC": [100, [31511.0], True, False],
+    "OCEAN": [1.22, [1.2], False, False],
+    "PEPE": [200, [0.00000830], False, False]
+  }
 }
