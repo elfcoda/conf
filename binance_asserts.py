3c4fa3a2c883113c2d159226f199aa4e390508ec
elfcoda <30581589+elfcoda@users.noreply.github.com>
2024-03-18 17:14:19 +0800

    Create binance_asserts.py

---
+++ binance_asserts.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+myAsserts = {
+  "BTC": [100, [31511.0], True, False],
+  "OCEAN": [1.22, [1.2], False, False],
+  "PEPE": [200, [0.00000830], False, False]
+}
